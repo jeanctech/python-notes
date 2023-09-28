63869ec6d0eaff0385ac8498420beded702960ae
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2023-09-28 18:51:57 -0500

    File update - File Changes - Py

--- Comments.py
+++ Comments.py
@@ -1,14 +1,17 @@
-''' Comentarios en Python '''
+# Comentarios en Python
 
 # Esto es Un Comentario en Una Sola Linea de Codigo
+
+''' Comentarios en Python '''
+
+# Esto es Un Comentario en Varias Lineas de Codigo
 
 '''Esto es un
 Comentario en
 Varias Lineas de Codigo'''
 
-# ---
+# Uso Correcto
 
-# Correcto
 a = 15  # Edad de María
 
 print(a)
